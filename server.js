39da85a095273d9d26ab21e28886b2ac140c6cc8
angie10 <delacruzvargasangelica26@gmail.com>
2022-10-06 21:04:12 -0400

    Succesfull index.liquid

--- server.js
+++ server.js
@@ -4,21 +4,25 @@ const express = require("express")
 const path = require("path") 
 const UserRouter = require('./controllers/userControllers')
 const marvelRouter = require('./controllers/marvelControllers')
+const commentRouter = require('./controllers/commentControllers')
 const middleware = require('./utils/middleware')
 
 // Express Application Object
-const app = express()
+// const app = express()
+const app = require('liquid-express-views')(express())
 
 // Middleware
 middleware(app)
 
 // HOME ROUTE
 app.get('/', (req, res) => {
-    res.send('Server is running')
+    // res.send('Server is running')
+    res.render('index.liquid')
 })
 
 // Register routes
 app.use('/marvel', marvelRouter)
+app.use('/comments', commentRouter)
 app.use('/users', UserRouter)
 
 // Server Listener
