e6ed5c3da41385890e648a824f4cd95702a17a7d
angie10 <delacruzvargasangelica26@gmail.com>
2022-10-18 08:12:27 -0400

    update

--- models/connection.js
+++ models/connection.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const mongoose = require('mongoose')
 
 // Database Connection
-const DEPLOYED_URL = process.env.DATABASE_URL
+const DEPLOYED_URL = process.env.DEPLOYED_URL
 
 const CONFIG = {
     useNewUrlParser: true,
